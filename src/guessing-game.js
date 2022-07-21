39c69d4a4b970fe15fa87a63a6437ee68652bf21
fogarea <shiitakedd@gmail.com>
2022-07-21 23:03:48 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minimum = 0;
+        this.maximum = 0;
+        this.middle = 0;
+    }
 
     setRange(min, max) {
-
+        this.minimum = min;
+        this.maximum = max
+        this.middle = Math.round((this.minimum + this.maximum) / 2);
     }
 
     guess() {
-
+        return this.middle;
     }
 
     lower() {
-
+        this.maximum = this.middle;
+        this.middle = Math.round((this.minimum + this.maximum) / 2)
     }
 
     greater() {
-
+        this.minimum = this.middle;
+        this.middle = Math.round((this.minimum + this.maximum) / 2)
     }
 }
 
